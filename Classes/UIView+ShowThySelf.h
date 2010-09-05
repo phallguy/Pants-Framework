2e2a1048552a613acde5f26ea55f68364569ffce
Paul Alexander <paul@xheo.com>
2010-09-05 04:01:07 -0700

    Fixed header license comments.

--- Classes/UIView+ShowThySelf.h
+++ Classes/UIView+ShowThySelf.h
@@ -1,10 +1,7 @@
-//
-//  UIView+ShowThySelf.h
-//  SelfShowView
-//
-//  Created by Paul Alexander on 9/4/10.
-//  Copyright 2010 n/a. All rights reserved.
-//
+// This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
+// Unported License. To view a copy of this license, visit 
+// http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative 
+// Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 
 #import <Foundation/Foundation.h>
 
--- Classes/UIView+ShowThySelf.m
+++ Classes/UIView+ShowThySelf.m
@@ -1,10 +1,8 @@
-//
-//  UIView+ShowThySelf.m
-//  SelfShowView
-//
-//  Created by Paul Alexander on 9/4/10.
-//  Copyright 2010 n/a. All rights reserved.
-//
+// This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
+// Unported License. To view a copy of this license, visit 
+// http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative 
+// Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+
 
 #import "UIView+ShowThySelf.h"
 
